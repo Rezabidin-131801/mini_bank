3ccb8a2966303f9dcfe949c5955ef2c510f67abe
Kristian Budi Pradana Putra <kristianbudipp@student.uns.ac.id>
2022-06-12 18:03:53 +0700

    HomeController_update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,49 @@ class HomeController extends Controller
     {
         return view('index');
     }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function ceksaldo()
+    {
+        return view('ceksaldo');
+    }
+
+    public function gantipin()
+    {
+        return view('gantipin');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function logout()
+    {
+        return view('logout');
+    }
+
+    public function services()
+    {
+        return view('services');
+    }
+
+    public function signform()
+    {
+        return view('signform');
+    }
+
+    public function signform2()
+    {
+        return view('signform2');
+    }
+
+    public function transfer()
+    {
+        return view('transfer');
+    }
 }
